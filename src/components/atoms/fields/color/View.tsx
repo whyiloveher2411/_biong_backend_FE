2fee01853ba066ec397b7539c28b798211a2769a
whyiloveher2411 <dangthuyenquan@gmail.com>
2026-02-12 21:40:08 +0700

    update view color

--- src/components/atoms/fields/color/View.tsx
+++ src/components/atoms/fields/color/View.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FieldViewItemProps } from '../type';
+import Typography from 'components/atoms/Typography';
 
 function View(props: FieldViewItemProps) {
-    return <>{props.content}</>;
+    return <Typography variant="body2" sx={{ color: props.content, fontWeight: 'bold' }}>{props.content}</Typography>;
 }
 
 export default View;
